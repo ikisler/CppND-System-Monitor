de09fd5992dd5432121f716c44a0e9f9a77e3143
Isabeau Kisler <ikisler@csumb.edu>
2020-12-07 22:23:02 -0800

    Cache some values for the system and grab those

--- src/system.cpp
+++ src/system.cpp
@@ -16,6 +16,11 @@ using std::size_t;
 using std::string;
 using std::vector;
 
+System::System() {
+  kernel_ = LinuxParser::Kernel();
+  os_ = LinuxParser::OperatingSystem();
+}
+
 // Return the system's CPU
 Processor& System::Cpu() { return cpu_; }
 
@@ -33,19 +38,13 @@ vector<Process>& System::Processes() {
 }
 
 // Return the system's kernel identifier (string)
-std::string System::Kernel() {
-  // consider saving this and returning the cached value
-  return LinuxParser::Kernel();
-}
+std::string System::Kernel() { return kernel_; }
 
 // Return the system's memory utilization
 float System::MemoryUtilization() { return LinuxParser::MemoryUtilization(); }
 
 // Return the operating system name
-std::string System::OperatingSystem() {
-  // consider saving this and returning the cached value
-  return LinuxParser::OperatingSystem();
-}
+std::string System::OperatingSystem() { return os_; }
 
 // Return the number of processes actively running on the system
 int System::RunningProcesses() { return LinuxParser::RunningProcesses(); }
